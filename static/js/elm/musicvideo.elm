fa58c2ae73c2a21e4cd71b81b9628c957df33f0f
Ian McCowan <imccowan@gmail.com>
2017-05-22 19:25:13 -0700

    very WIP styling

--- static/js/elm/musicvideo.elm
+++ static/js/elm/musicvideo.elm
@@ -4,8 +4,11 @@ import AnimationFrame
 import Array exposing (Array)
 import Platform.Cmd exposing ((!))
 import Html exposing (Html)
+import Svg exposing (Svg)
+import Svg.Attributes as SvgAttr
 import Task
 import Time exposing (Time)
+
 import Lyrics exposing (..)
 
 
@@ -15,15 +18,65 @@ type Msg
 
 
 type ModelMsg
-    = PlayState Bool
+    = SetLyricSizes SizedLyricBook
+    | PlayState Bool
     | Animate
     | NoOp
 
 
+type alias Size =
+    { height : Float
+    , width : Float
+    }
+
+
+type alias Position =
+    { x : Float
+    , y : Float
+    }
+
+
+type alias Sized t =
+    { size : Size
+    , content : t
+    }
+
+
+type alias SvgData =
+    { attrs : List (Svg.Attribute Msg)
+    , elements : SvgElements
+    }
+
+
+type SvgElements =
+    SvgElements (List (Located SvgData))
+
+
+type alias Positioned t =
+    { t | pos : Position }
+
+
+type alias Located t =
+    Positioned (Sized t)
+
+
+type alias SizedLyricPage =
+    Sized (List (SizedLyricsLine))
+
+
+type alias SizedLyricsLine =
+    Sized (List (Sized Lyric))
+
+
+type alias SizedLyricBook =
+    List SizedLyricPage
+
+
 type alias Model =
     { playhead : Time
-    , page : Maybe LyricPage
+    , page : Maybe (SizedLyricPage)
     , playing : Bool
+    , lyrics : SizedLyricBook
     }
 
 
@@ -32,18 +85,109 @@ init =
     { playhead = 0.0
     , page = Nothing
     , playing = False
+    , lyrics = []
     }
-        ! [ Cmd.none ]
+    ! [ getSizes lyrics ]
 
 
-flattenPage : Time -> LyricPage -> List String
-flattenPage time page =
+pageLines : Time -> SizedLyricPage -> List String
+pageLines time page =
     List.map
-        (List.map .text << List.filter (Maybe.withDefault False << lyricBefore time)) page
+        (List.map .text << List.filter
+             <| Maybe.withDefault False << lyricBefore time) page
         |> List.map (String.join "")
 
 
-simpleDisplay : Time -> Maybe LyricPage -> Html Msg
+nextLyricView : Sized Lyric -> Located (Svg Msg) -> Located (Svg Msg)
+nextLyricView lyric prevSvg =
+    Svg.svg
+        []
+        [ Svg.text_
+              [ SvgAttr.x prevSvg.x + prevSvg.content.width
+              , SvgAttr.y prevSvg.y
+              ]
+              [ Svg.text lyric.text ]
+        ]
+
+
+firstLyricSvg : Sized Lyric -> Located SvgData
+firstLyricSvg lyric =
+    { content =
+          { attrs =
+                [ SvgAttr.x "0"
+                , SvgAttr.y "0"
+                ]
+          , elements =
+              [ { attrs = []
+                , elements = SvgElements ()
+              ]
+          }
+    , pos = Position 0 0
+    , size = lyric.size
+    }
+
+
+lyricLineView : SizedLyricsLine -> List (Located SvgData)
+lyricLineView line =
+    case Maybe.map2 (,) (List.head line.content) (List.tail line.content) of
+        Just ( first, rest ) ->
+            List.scanl
+                nextLyricView
+                (firstLyricSvg first)
+                rest
+
+        Nothing ->
+            Svg.svg [] []
+
+
+firstLineSvg : SizedLyricsLine -> Located SvgData
+firstLineSvg line =
+    { content =
+          { attrs =
+                [ SvgAttr.x "0"
+                , SvgAttr.y "0"
+                ]
+          , elements =
+              SvgElements (lyricLineView line)
+          }
+    , pos = Position 0 0
+    , size = line.size
+    }
+
+
+nextLineView : SizedLyricsLine -> Located SvgData -> Located SvgData
+nextLineView line prevSvg =
+    let
+        pos =
+            Position 0 (prevSvg.pos.y + prevSvg.size.height)
+    in
+        { prevSvg
+            | pos = pos
+            , size = line.size
+            , content =
+              { attrs =
+                    [ SvgAttr.x <| toString pos.x
+                    , SvgAttr.y <| toString pos.y ]
+              , elements =
+                  SvgElements (lyricLineView line)
+              }
+        }
+
+
+lyricPageView : SizedLyricPage -> List (Located SvgData)
+lyricPageView page =
+    case Maybe.map2 (,) (List.head page.content) (List.tail page.content) of
+        Just ( first, rest ) ->
+            List.scanl
+                nextLineView
+                (firstLineSvg first)
+                rest
+
+        Nothing ->
+            [ ]
+
+
+simpleDisplay : Time -> Maybe SizedLyricPage -> Html Msg
 simpleDisplay time mpage =
     case mpage of
         Nothing ->
@@ -52,17 +196,15 @@ simpleDisplay time mpage =
         Just page ->
             Html.div
                 []
-                <| List.map
-                    (Html.p [] << List.singleton << Html.text)
-                    (flattenPage time page)
+                lyricPageView page
 
 
 view : Model -> Html Msg
 view model =
     Html.div
         []
+        
         [ simpleDisplay model.playhead model.page
-        , Html.text <| toString <| Time.inSeconds model.playhead
         ]
 
 
@@ -71,27 +213,32 @@ last l =
     List.head <| List.reverse l
 
 
-lyricBefore : Time -> Lyric -> Maybe Bool
-lyricBefore t lyric =
-    Just (lyric.time < t)
+lyricBefore : Time -> Sized Lyric -> Maybe Bool
+lyricBefore t token =
+    Just (token.content.time < t)
 
 
-pageBefore : Time -> LyricPage -> Bool
-pageBefore t page =
-    List.head page
-        |> Maybe.andThen List.head
+pageIsBefore : Time -> SizedLyricPage -> Bool
+pageIsBefore t page =
+    List.head page.content
+        |> Maybe.andThen (.content >> List.head)
         |> Maybe.andThen (lyricBefore t)
         |> Maybe.withDefault False
 
 
-findPage : LyricBook -> Time -> Maybe LyricPage
-findPage lyrics time =
-    last <| List.filter (pageBefore time) lyrics
+findPage : SizedLyricBook -> Time -> Maybe SizedLyricPage
+findPage book time =
+    last <| List.filter (pageIsBefore time) book
 
 
 updateModel : ModelMsg -> Time -> Model -> Model
 updateModel msg time model =
     case msg of
+        SetLyricSizes sizedLyrics ->
+            { model
+                  | lyrics = sizedLyrics
+            }
+
         PlayState playing ->
             { model
                 | playing = playing
@@ -104,7 +251,7 @@ updateModel msg time model =
             in
                 { model
                     | playhead = newTime
-                    , page = findPage lyrics newTime
+                    , page = findPage model.lyrics newTime
                 }
 
         NoOp ->
@@ -122,14 +269,19 @@ update msg model =
                 ! [ Cmd.none ]
 
 
-port state : (Bool -> msg) -> Sub msg
+port playState : (Bool -> msg) -> Sub msg
+port gotSizes : (SizedLyricBook -> msg) -> Sub msg
+
+
+port getSizes : LyricBook -> Cmd msg
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
         [ AnimationFrame.diffs (WithTime Animate)
-        , state (AtTime << PlayState)
+        , playState (AtTime << PlayState)
+        , gotSizes (AtTime << SetLyricSizes)
         ]
 
 
